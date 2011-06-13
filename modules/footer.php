6dd7ad8d1c313915d4f2748028c2d003d31be881
Datermine <g@datermine.com>
2011-06-13 15:05:50 -0400

    Remove copyright from footer

--- modules/footer.php
+++ modules/footer.php
@@ -1,5 +1,5 @@
 <div data-role="footer">
     <?php require_once('modules/footer_actions.php') ?>
     <a href="http://www.dosomething.org/staples-for-students/run-a-drive">&rarr; GO TO WEBSITE</a>
-    <span id="copyright">Copyright &copy; 2011</span>
+    <!-- <span id="copyright">Copyright &copy; 2011</span> -->
 </div><!-- /footer -->
