087cb0faf53be688a4e79c2ebac41a526aa02664
Jim Yi <jim@jimyi.com>
2011-06-13 14:48:32 -0400

    Organize/format/cleanup code

--- conn.php
+++ conn.php
@@ -1,10 +1,10 @@
-<?
+<?php
+
+// TODO: use mysqli_ or PDO
+
 $con = mysql_connect("localhost","root","ZanzibaR1");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
   
-  mysql_select_db("jqdev", $con) or die('Could not connect');
-  
-  ?>
+mysql_select_db("jqdev", $con) or die('Could not connect');
