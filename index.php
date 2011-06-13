11caa13362c5de3f143d65ce80113a96fd297639
Jim Yi <jim@jimyi.com>
2011-06-13 15:04:29 -0400

    add index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>jQuery Mobile Tutorial </title>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
+    <script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
+</head>
+<body>
+
+<div data-role="page">
+
+    <div data-role="header">
+        <h1>Landing Page</h1>
+    </div><!-- /header -->
+
+    <div data-role="content">
+        <p>Overview of the project</p>
+		<p>PSA Video</p>
+    </div><!-- /content -->
+
+    <?php require_once('modules/footer.php') ?>
+</div><!-- /page -->
+
+</body>
+</html>
