9d0c6902aac08803fabd7690606420a80f38cc96
Rob Hawkins <rhawkins@dosomething.org>
2011-06-13 16:22:51 -0500

    Updated the contest pagE

--- contest.php
+++ contest.php
@@ -15,8 +15,32 @@
 		</div><!-- /header -->
 
 		<div data-role="content">
-			<p>Contest1</p>
-			<p>Contest2</p>
+		<h2>Tell 5 Friends</h2>
+		<p>We need your help building our Staples for Students team! Please list the email of 5 
+		friends who participated in this campaign with you. You'll be automatically entered to win one of our awesome campaign giveaways, like signed swag or t-shirts! </p>
+			
+		<form action="contest.php" method="post">
+			<div data-role="fieldcontain" >
+				<label for="name">Your Email Address</label>
+				<input type="text" name="name" id="name" />
+			</div>
+            <div data-role="fieldcontain">
+				<label for="name">School</label>
+				<input type="text" name="school" id="school" />
+			</div>
+			<div data-role="fieldcontain">
+				<label for="name">Email</label>
+				<input type="text" name="email" id="email" required="required" placeholder="This field requires an e-mail address." />
+			</div>
+			<div data-role="fieldcontain">
+				<label for="name">Phone</label>
+				<input type="text" name="phone" id="phone" value="" />
+			</div>
+			<div data-role="fieldcontain">
+				<label for="name">Zip</label>
+				<input type="text" name="zip" id="zip" value="" size="5" required="required" placeholder="This field is required." />
+			</div>
+	
 		</div><!-- /content -->
 
 		<?php require_once('modules/footer.php') ?>
