b968efdd21de91341776bbffa99ffa5acb2f50b2
Datermine <g@datermine.com>
2011-06-14 13:53:43 -0400

    Modify the footer actions

--- modules/footer_actions.php
+++ modules/footer_actions.php
@@ -1,7 +1,7 @@
 
 <fieldset class="ui-grid-c">
-    <a href="sign_up.php" data-role="button" data-icon="plus" data-theme="a">SIGNUP</a>
+    <a href="sign_up.php" data-role="button" data-icon="plus" data-theme="a">SIGN UP</a>
     <a href="report_back.php" data-role="button"  data-icon="back" data-iconpos="right" data-theme="b">REPORT BACK</a>
-    <a href="action_tips.php" data-role="button"  data-icon="info" data-theme="c">ACTION TIPS</a>
+    <a href="action_tips.php" data-role="button"  data-icon="info" data-theme="c">TIPS</a>
     <a href="contest.php" data-role="button" data-icon="star" data-theme="b">CONTEST</a>
 </fieldset>
