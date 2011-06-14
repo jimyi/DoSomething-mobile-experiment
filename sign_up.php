8ec82afa5732db2c131e3fa675d3e8efd14b889f
Jim Yi <jim@jimyi.com>
2011-06-14 14:27:49 -0400

    working signup page

--- sign_up.php
+++ sign_up.php
@@ -18,19 +18,19 @@
                     <input type="text" name="name" id="name" />
                 </div>
                 <div data-role="fieldcontain">
-                    <label for="name">School</label>
+                    <label for="school">School</label>
                     <input type="text" name="school" id="school" />
                 </div>
                 <div data-role="fieldcontain">
-                    <label for="name">Email</label>
+                    <label for="email">Email</label>
                     <input type="text" name="email" id="email" required="required" placeholder="This field requires an e-mail address." />
                 </div>
                 <div data-role="fieldcontain">
-                    <label for="name">Phone</label>
+                    <label for="phone">Phone</label>
                     <input type="text" name="phone" id="phone" value="" />
                 </div>
                 <div data-role="fieldcontain">
-                    <label for="name">Zip</label>
+                    <label for="zip">Zip</label>
                     <input type="text" name="zip" id="zip" value="" size="5" required="required" placeholder="This field is required." />
                 </div>
 
--- sign_up_action.php
+++ sign_up_action.php
@@ -1,4 +1,40 @@
 <?php require_once("common.php"); ?>
+<?php
+
+// Go through our form inputs and set defaults
+$input = array();
+$input['name'] = isset($_POST['name']) ? $_POST['name'] : '';
+$input['school'] = isset($_POST['school']) ? $_POST['school'] : '';
+$input['email'] = isset($_POST['email']) ? $_POST['email'] : '';
+$input['phone'] = isset($_POST['phone']) ? $_POST['phone'] : '';
+$input['zip'] = isset($_POST['zip']) ? $_POST['zip'] : '';
+
+// TODO: validate the input here
+if (empty($input['email'])) {
+    // do something
+}
+
+// Add the user's entry to the database.
+
+// Use PDO's prepared statements for security.
+$queryString = 'INSERT INTO staples_signups (name, school, email, phone, zip) ';
+$queryString .= 'VALUES (:name, :school, :email, :phone, :zip);';
+$query = $db->prepare($queryString);
+
+// Bind all of our selected input parameters.
+foreach ($input as $column => $value) {
+    $query->bindValue(':' . $column, $value);
+}
+
+// Execute our prepared statement.  We don't particularly care about the result.
+try {
+    $query->execute();
+    $success = TRUE;
+} catch (PDOException $e) {
+    $success = FALSE;
+}
+
+?>
 <html>
 <head>
     <title>Sign Up</title>
@@ -12,9 +48,11 @@
 		</div><!-- /header -->
 
 		<div data-role="content">
-			<?php // TODO: AHH MYSQL INJECTION ?>
-			<?php //mysql_query("Insert into signup_staples values('','$_POST[name]','$_POST[school]','$_POST[email]','$_POST[phone]',$_POST[zip])"); ?>
-			You have been signed up!
+            <?php if ($success): ?>
+                You have been signed up!
+            <?php else: ?>
+                Error signing up!  Please try again.
+            <?php endif; ?>
 		</div><!-- /content -->
 
 		<?php require_once('modules/footer.php') ?>
